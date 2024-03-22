c2c93a9e00dc26633383bf2afac47c97d939f96c
sheheemmulakkal <shaimonsheheem@gmail.com>
2024-03-22 16:27:01 +0530

    feat: Implement admin login and signup functionality
    This commit finalizes the development of the admin login and signup features in the backend. Admin users can now securely log in to the system and register new accounts.

--- src/controller/adminController.ts
+++ src/controller/adminController.ts
@@ -1,8 +1,26 @@
 import { Request, Response } from "express";
+import { Admin } from "../models/adminModel";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-const adminSignup = (req: Request, res: Response) => {
+const adminSignup = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
+    const existingAdmin = await Admin.findOne({ email: email.trim() });
+    if (existingAdmin) {
+      return res.status(400).json({ message: "Admin Already exist" });
+    }
+    const hashedPassword = await bcrypt.hash(password.trim(), 12);
+    const admin = new Admin({
+      email: email.trim(),
+      password: hashedPassword,
+    });
+    const response = await admin.save();
+    const token = jwt.sign(
+      { role: "admin", id: response._id },
+      process.env.JWT_KEY!
+    );
+    res.status(200).json({ message: "Admin signin successfully", token });
   } catch (error) {
     console.log(error);
     if (error instanceof Error) {
@@ -11,4 +29,28 @@ const adminSignup = (req: Request, res: Response) => {
   }
 };
 
-export { adminSignup };
+const adminLogin = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const isExist = await Admin.findOne({ email: email.trim() });
+    if (!isExist) return res.status(400).json({ message: "Email not exist" });
+    const validPassword = await bcrypt.compare(
+      password.trim(),
+      isExist.password
+    );
+    if (!validPassword)
+      return res.status(400).json({ message: "Inavlid password" });
+
+    const token = jwt.sign(
+      { role: "admin", id: isExist._id },
+      process.env.JWT_KEY!
+    );
+    res.status(200).json({ message: "Admin logged in", token });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).send(error.message);
+    }
+  }
+};
+
+export { adminSignup, adminLogin };
--- src/routes/adminRouter.ts
+++ src/routes/adminRouter.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { submitForm } from "../controller/formController";
+import { adminLogin, adminSignup } from "../controller/adminController";
 
 const router = express.Router();
 
 router.post("/submit-form", submitForm);
+router.post("/login", adminLogin);
+router.post("/signup", adminSignup);
 
 export default router;
